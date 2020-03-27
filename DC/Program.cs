9d95ad0a05b7f0c45db82d7a0ac90d01d457df8a
Marina_Bor <marinabor99@mail.ru>
2020-03-27 13:15:38 +0300

    Version 1.2

--- DC/Program.cs
+++ DC/Program.cs
@@ -7,28 +7,38 @@ namespace DC
         static void Main(string[] args)
         {
             Dictionary<string, string> some_dictionary = new Dictionary<string, string>();
-          
-            Console.WriteLine("Enter 1 note");
-            string mess1 = Console.ReadLine();
+            var key = 'y';
+            string note, item, value;
 
-            Console.WriteLine("Enter 2 note");
-            string mess2 = Console.ReadLine();
-           
-            Console.WriteLine("Enter 3 note");
-            string mess3 = Console.ReadLine();
-           
-            Console.WriteLine("Enter 4 note");
-            string mess4 = Console.ReadLine();
-          
-            Console.WriteLine("Enter 5 note");
-            string mess5 = Console.ReadLine();
-            
-            some_dictionary.Add(mess1.Substring(0, mess1.IndexOf(' ')), mess1.Substring(mess1.IndexOf(' ') + 1));
-            some_dictionary.Add(mess2.Substring(0, mess2.IndexOf(' ')), mess2.Substring(mess2.IndexOf(' ') + 1));
-            some_dictionary.Add(mess3.Substring(0, mess3.IndexOf(' ')), mess3.Substring(mess3.IndexOf(' ') + 1));
-            some_dictionary.Add(mess4.Substring(0, mess4.IndexOf(' ')), mess4.Substring(mess4.IndexOf(' ') + 1));
-            some_dictionary.Add(mess5.Substring(0, mess5.IndexOf(' ')), mess5.Substring(mess5.IndexOf(' ') + 1));
+            while (key == 'y')
+            {
+                Console.WriteLine("Enter? y/n");
+                key = Convert.ToChar(Console.ReadLine());
 
+                if (key == 'y')
+                {
+                    Console.WriteLine("Enter:");
+                    note = Console.ReadLine();
+
+                    item = note.Substring(0, note.IndexOf(' '));
+                    value = note.Substring(note.IndexOf(' ') + 1);
+
+                    if (some_dictionary.ContainsKey(item) || some_dictionary.ContainsValue(value))
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Success");
+                        some_dictionary.Add(item, value);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+          
             foreach (KeyValuePair<string, string> pair in some_dictionary)
             {
                 Console.WriteLine($" {pair.Key} - {pair.Value}");
