6e8f04b8088aded4ceaa8356727e4deb59ec8b71
MarinaBorisova <marinabor99@mail.ru>
2020-03-30 13:14:17 +0300

    ProductManager

---
+++ Dictionary-master/DC/ProductManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DC
+{
+    class ProductManager
+    {
+        string item, valuea;
+        private Dictionary<string, string> ProductDictionary = new Dictionary<string, string>();
+
+        public string this[string key]
+        {
+            get
+            {
+                return ProductDictionary[key];
+            }
+            set
+            {
+                if (!ProductDictionary.ContainsKey(item) || !ProductDictionary.ContainsValue(valuea))
+                {
+                    ProductDictionary[key] = value;
+                }
+                else
+                {
+                    ProductDictionary[key] = null;
+                }
+            }
+        }
+
+        public void AddProduct(string Note)
+        {
+           item = Note.Substring(0, Note.IndexOf(' '));
+           valuea = Note.Substring(Note.IndexOf(' ') + 1);
+
+
+            if (ProductDictionary.ContainsKey(item) || ProductDictionary.ContainsValue(valuea))
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                ProductDictionary.Add(item, valuea);
+                Console.WriteLine("Success");
+            }
+        }
+
+        public void SeeDictionary()
+        {
+            foreach (KeyValuePair<string, string> pair in ProductDictionary)
+            {
+                Console.WriteLine($" {pair.Key} - {pair.Value}");
+            }
+        }
+    }
+}
---
+++ Dictionary-master/DC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic; 
+
+
+
+namespace DC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = 'y';
+            string note;
+
+            ProductManager productManager = new ProductManager();
+
+            while (key == 'y')
+            {
+                Console.WriteLine("Enter? y/n");
+                key = Convert.ToChar(Console.ReadLine());
+
+                if (key == 'y')
+                {
+                    Console.WriteLine("Enter:");
+                    note = Console.ReadLine();
+
+                    productManager.AddProduct(note);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            productManager.SeeDictionary();
+            Console.ReadLine();
+        }
+    }
+}
