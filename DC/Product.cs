28f382b01994a86eb855efae577ad965420a5f0a
MarinaBorisova <marinabor99@mail.ru>
2020-03-30 14:34:28 +0300

    Add Product.cs

---
+++ DC/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DC
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string NameProduct { get; set; }
+    }
+}
