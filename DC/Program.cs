a513b60f1327612ebedcd61b1005f6b9e26925f4
MarinaBorisova <marinabor99@mail.ru>
2020-03-30 14:15:05 +0300

    New version

--- DC/Program.cs
+++ DC/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
+
+
+
 namespace DC
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> some_dictionary = new Dictionary<string, string>();
             var key = 'y';
-            string note, item, value;
+            string note;
+
+            ProductManager productManager = new ProductManager();
 
             while (key == 'y')
             {
@@ -20,29 +24,14 @@ namespace DC
                     Console.WriteLine("Enter:");
                     note = Console.ReadLine();
 
-                    item = note.Substring(0, note.IndexOf(' '));
-                    value = note.Substring(note.IndexOf(' ') + 1);
-
-                    if (some_dictionary.ContainsKey(item) || some_dictionary.ContainsValue(value))
-                    {
-                        Console.WriteLine("Error");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Success");
-                        some_dictionary.Add(item, value);
-                    }
+                    productManager.AddProduct(note);
                 }
                 else
                 {
                     break;
                 }
             }
-          
-            foreach (KeyValuePair<string, string> pair in some_dictionary)
-            {
-                Console.WriteLine($" {pair.Key} - {pair.Value}");
-            }
+            productManager.SeeDictionary();
             Console.ReadLine();
         }
     }
