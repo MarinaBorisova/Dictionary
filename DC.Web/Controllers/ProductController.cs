79ade874ade757c6b8ebf79159465829a5b1c51c
MarinaBorisova <marinabor99@mail.ru>
2020-03-31 17:02:05 +0300

    add ProductModule

---
+++ DC.Web/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DC.Core;
+
+namespace DC.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DC.Web/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DC.Web.Models
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public string NameProduct { get; set; }
+
+        public ProductModel(string id, string name)
+        {
+            Id = id;
+            NameProduct = name;
+        }
+    }
+}
