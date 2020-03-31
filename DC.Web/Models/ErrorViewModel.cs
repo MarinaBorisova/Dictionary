4d646c51f26abe0adffafa533e863ba942eb503a
MarinaBorisova <marinabor99@mail.ru>
2020-03-31 15:45:13 +0300

    New project DC.Web

---
+++ DC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
